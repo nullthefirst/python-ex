2de3e35d4d76fc31d839fa72145a6ec9cb685392
Usheninte Dangana <usheninte234@gmail.com>
2025-03-03 07:35:23 +0100

    Add me to the queue

--- chaitanas-colossal-coaster/list_methods.py
+++ chaitanas-colossal-coaster/list_methods.py
@@ -11,7 +11,14 @@ def add_me_to_the_queue(express_queue, normal_queue, ticket_type, person_name):
     :return: list - the (updated) queue the name was added to.
     """
 
-    pass
+    updated_queue = None
+
+    if ticket_type == 1:
+        updated_queue = express_queue.append(person_name)
+    else:
+        updated_queue = normal_queue.append(person_name)
+
+    return updated_queue
 
 
 def find_my_friend(queue, friend_name):
