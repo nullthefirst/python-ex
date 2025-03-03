7a6b40e8126810e49731abdf35ca9011f509a0e1
Usheninte Dangana <usheninte234@gmail.com>
2025-03-03 08:21:13 +0100

    Completed leap year logic

--- leap/leap.py
+++ leap/leap.py
@@ -12,4 +12,7 @@ def leap_year(year):
     if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
         return True
     else:
-        return False
+        if year % 4 == 0 and year % 100 != 0:
+            return True
+        else:
+            return False
