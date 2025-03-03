581a45753f5e7879a072b6c330b275bd31e32d9a
Usheninte Dangana <usheninte234@gmail.com>
2025-03-03 08:02:55 +0100

    Added docstrings to leap year function

---
+++ leap/leap.py
@@ -0,0 +1,12 @@
+def leap_year(year):
+    """Determine if input year is a leap year based off the rules below.
+
+    A leap year (in the Gregorian calendar) occurs:
+    - In every year that is evenly divisible by 4.
+    - Unless the year is evenly divisible by 100, in which case it's only a leap year if the year is also evenly divisible by 400.
+
+    Args:
+        year (int): year to be assessed per leap year logic.
+    """
+
+
