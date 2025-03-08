f9678cb3fe9baf8d7b8f1212cdd10972e4851259
Usheninte Dangana <usheninte234@gmail.com>
2025-03-08 13:47:31 +0100

    Completed grain amounts on set board squares

--- grains/grains.py
+++ grains/grains.py
@@ -2,7 +2,16 @@ def square(number):
     if number <=0 or number > 64:
         raise ValueError("square must be between 1 and 64")
     else:
-        pass
+        positions = [n for n in range(1, 65)]
+        board = {1: 1}
+
+        for i in range(2, 65):
+            value = board[i - 1] * 2
+            board[i] = value
+
+        return board[number]
 
 def total():
     pass
+
+# print(square(4))
