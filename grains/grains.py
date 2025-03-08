e7ba58fb38f4630599bc7297d94b3cabe549eb33
Usheninte Dangana <usheninte234@gmail.com>
2025-03-08 13:52:37 +0100

    Completed total number of grains implementation

--- grains/grains.py
+++ grains/grains.py
@@ -1,17 +1,21 @@
+def bounty():
+    positions = [n for n in range(1, 65)]
+    board = {1: 1}
+
+    for i in range(2, 65):
+        value = board[i - 1] * 2
+        board[i] = value
+
+    return board
+
 def square(number):
     if number <=0 or number > 64:
         raise ValueError("square must be between 1 and 64")
     else:
-        positions = [n for n in range(1, 65)]
-        board = {1: 1}
 
-        for i in range(2, 65):
-            value = board[i - 1] * 2
-            board[i] = value
-
-        return board[number]
+        return bounty()[number]
 
 def total():
-    pass
+    return sum(bounty().values())
 
-# print(square(4))
+print(total())
