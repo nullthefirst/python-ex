22bc9f9d150a441764e0f6c4e01c4b731ccf4bcb
Usheninte Dangana <usheninte234@gmail.com>
2025-03-08 14:10:05 +0100

    Completely handled questions

--- bob/bob.py
+++ bob/bob.py
@@ -1,5 +1,7 @@
 def response(hey_bob):
-    if hey_bob.endswith("?"):
+    if hey_bob.isupper() and hey_bob.endswith("?"):
+        return "Calm down, I know what I'm doing!"
+    elif hey_bob.endswith("?"):
         return "Sure."
     else:
         return "Whatever."
