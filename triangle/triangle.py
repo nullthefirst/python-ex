f6042a7a1374cda39b8f4b38c2e4deacbaffac9d
Usheninte Dangana <usheninte234@gmail.com>
2025-03-15 13:51:11 +0100

    Added validate function

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,5 +1,18 @@
+def validate(sides):
+    a, b, c = sides
+
+    valid = None
+
+    if a == 0 or b == 0 or c == 0:
+        valid = False
+    else:
+        valid = True
+
+    return valid
+
+
 def equilateral(sides):
-    return True if sides[0] == sides[1] == sides[2] else False
+    return validate(sides) and (True if sides[0] == sides[1] == sides[2] else False)
 
 
 def isosceles(sides):
