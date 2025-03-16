ac1c2f1c3fce1d2d3bd9920e70e7369ae79f642c
Usheninte Dangana <usheninte234@gmail.com>
2025-03-16 11:35:29 +0100

    Completed darts exercise

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,14 @@
+import math
+
 def score(x, y):
-    pass
+    distance = math.sqrt((x - 0) ** 2 + (y - 0) ** 2)
+
+    if distance <= 1:
+        return 10
+    elif distance <= 5:
+        return 5
+    elif distance <= 10:
+        return 1
+    else:
+        return 0
+
