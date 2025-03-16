505c84cbb53b6ce90e5d37dc2b906272463514ee
Usheninte Dangana <usheninte234@gmail.com>
2025-03-16 17:33:50 +0100

    Completed pangram exercise

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -3,29 +3,15 @@ import string, re
 def is_pangram(sentence):
     alphabet = {letter: 0 for letter in string.ascii_lowercase}
 
-    print(sentence)
-
     sentence_lower = sentence.lower()
+
     sentence_chars = re.sub(r'\s+', '', sentence_lower)
 
     for char in sentence_chars:
         if char in alphabet.keys():
             alphabet[char] += 1
 
-    print(alphabet)
+    alphabet_list = [[key, value] for key, value in alphabet.items() if value > 0]
 
-    verdict = False
+    return False if len(alphabet_list) < 26 else True
 
-    for key, value in alphabet.items():
-        if value == 0:
-            verdict = False
-        else:
-            verdict = True
-
-    return verdict
-
-
-text = "The quick brown fox jumps 019 over the lazy dog."
-text_2 = "five boxing wizards jump quickly at it"
-
-print(is_pangram(text_2))
