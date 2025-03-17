7b96cfe81b987d5b53ee1df166298fa4cc812bdd
Usheninte Dangana <usheninte234@gmail.com>
2025-03-17 12:22:34 +0100

    Completed armstrong numbers exercise

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,10 @@
 def is_armstrong_number(number):
-    pass
+    num_string = "{}".format(number)
+
+    checker = 0
+
+    for char in num_string:
+        checker += int(char) ** len(num_string)
+
+    return True if checker == int(number) else False
+
