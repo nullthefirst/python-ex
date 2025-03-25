f4f2ce8d4668ac574c0323751a11ee2e89709355
Ninte Dangana <nintedangana@gmail.com>
2025-03-25 15:21:45 +0100

    Implemented rule 1 functionality

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -1,2 +1,19 @@
+def rule_1(data):
+    starter = ["a", "e", "i", "o", "u", "xr", "yt"]
+
+    verdict = None
+
+    for chars in starter:
+        if data.lower().startswith(chars):
+            verdict = True
+
+    return verdict
+
+
 def translate(text):
-    pass
+    output = text
+
+    if rule_1(text):
+        output += "ay"
+
+    return output
