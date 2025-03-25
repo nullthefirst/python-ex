cebb825612844071b6355df60683f0141bd65b66
Ninte Dangana <nintedangana@gmail.com>
2025-03-25 15:36:47 +0100

    Worked on rule 2 functionality

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -10,10 +10,40 @@ def rule_1(data):
     return verdict
 
 
+def rule_2(data):
+    vowels = ["a", "e", "i", "o", "u"]
+
+    chars = list(data)
+
+    word = []
+    verdict = None
+
+    for letter in chars:
+        if letter not in vowels:
+            pass
+        else:
+            vowel_start = chars.index(letter)
+
+            # reorder word starting with vowel
+            word += chars[vowel_start:]
+            # attach constants to word end
+            word += chars[:vowel_start]
+
+            verdict = True
+
+    "".join(word)
+    word += "ay"
+    print(word)
+
+    return [verdict, word]
+
+
 def translate(text):
     output = text
 
     if rule_1(text):
         output += "ay"
+    elif rule_2(text)[0]:
+        output = rule_2(text)[1]
 
     return output
