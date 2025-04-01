c1d2e0e89e11a8d0e2055272edc4ce1dba352276
Ninte Dangana <nintedangana@gmail.com>
2025-04-01 18:42:44 +0100

    Completed rna transcription exercise

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -2,4 +2,18 @@ def to_rna(dna_strand):
     if dna_strand == "":
         return ""
 
+    conversion = {
+        "G": "C",
+        "C": "G",
+        "T": "A",
+        "A": "U"
+    }
 
+    rna_strand = ""
+
+    for char in dna_strand:
+        for key, value in conversion.items():
+            if char == key:
+                rna_strand += value
+
+    return rna_strand
