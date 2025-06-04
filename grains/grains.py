aa02797ef372e3805ceded4b3261f80027c0d953
Usheninte Dangana <usheninte234@gmail.com>
2025-06-04 06:44:49 +0100

    Completed function to determine number of grains on chess square

--- grains/grains.py
+++ grains/grains.py
@@ -1,13 +1,17 @@
-def square(number):
-    if number < 1 or number > 64:
-        raise ValueError("square must be between 1 and 64")
-
+def grains():
     output = [1]
 
     for index in range(1, 65):
         output.append(output[index - 1] * 2)
 
-    return output[number - 1]
+    return output
+
+
+def square(number):
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+
+    return grains()[number - 1]
 
 
 def total():
