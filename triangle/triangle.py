296db72d1984892d3814d68a4cc9b7b9117dc055
Usheninte Dangana <usheninte234@gmail.com>
2025-06-05 06:36:39 +0100

    Added function to verify is input match triangle rules. Worked on equilateral triangle logic.

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,5 +1,24 @@
+def istriangle(sides):
+    a, b, c = sides
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return False
+    elif (a + b < c) or (b + c < a) or (c + a < b):
+        return False
+    else:
+        return True
+
+
 def equilateral(sides):
-    pass
+    a, b, c = sides
+
+    if istriangle(sides):
+        if a == b and b == c and a == c:
+            return True
+        else:
+            return False
+    else:
+        return False
 
 
 def isosceles(sides):
