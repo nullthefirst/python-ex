53748ab3eb2e0fac19e3a38ccb69baea9d2d7bbb
Usheninte Dangana <usheninte234@gmail.com>
2025-06-09 14:19:47 +0100

    Completed Collatz Conjecture exercise

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,23 @@
 def steps(number):
-    pass
+    steps = 0
+
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+    elif number == 1:
+        return steps
+    else:
+        state = number
+
+        while state > 1:
+            if state % 2 == 0:
+                state = state / 2
+                steps += 1
+            else:
+                state = (state * 3) + 1
+                steps += 1
+
+        return steps
+
+
+output = steps(16)
+print(output)
