cc24f528e18536a3a97f1f882bfd68359fa34824
Usheninte Dangana <usheninte234@gmail.com>
2025-06-17 18:15:05 +0100

    Solved Bob exercise - except from edge cases.

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,20 @@
+import re
+
+
 def response(hey_bob):
-    pass
+    if len(hey_bob.strip()) == 0:
+        return "Fine. Be that way!"
+    elif len(hey_bob) > 1:
+        if hey_bob.isupper():
+            if hey_bob.endswith("?"):
+                return "Calm down, I know what I'm doing!"
+            else:
+                return "Whoa, chill out!"
+        else:
+            if hey_bob[0] == " ":
+                return "Whatever."
+            else:
+                return "Sure."
+
+
+response("SDSVDV")
