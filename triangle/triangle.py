fee666cd95e2468aa9c6ecc20af83a7d84b7d00f
Nintey Dangana <usheninte234@gmail.com>
2025-12-19 13:22:01 +0100

    Added logic validation functions

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,3 +1,20 @@
+def is_valid_lengths(sides):
+    a, b, c = sides
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return False
+    else:
+        return True
+
+def is_differing_sides(sides):
+    a, b, c = sides
+
+    if a + b >= c and b + c >= a and a + c >= b:
+        return True
+    else:
+        return False
+
+
 def equilateral(sides):
     pass
 
