2a52eb8b35e2ae4e265c74ace8980e299edc38e8
Usheninte Dangana <usheninte234@gmail.com>
2025-12-20 10:52:24 +0100

    Completed Check for criticality task

--- meltdown-mitigation/conditionals.py
+++ meltdown-mitigation/conditionals.py
@@ -14,7 +14,10 @@ def is_criticality_balanced(temperature, neutrons_emitted):
     - The product of temperature and neutrons emitted per second is less than 500000.
     """
 
-    pass
+    if temperature < 800 and neutrons_emitted > 500 and temperature * neutrons_emitted < 500000:
+        return True
+    else:
+        return False
 
 
 def reactor_efficiency(voltage, current, theoretical_max_power):
