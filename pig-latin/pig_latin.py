1b4540ab985dcd9904eda18b4551106fc0eeb293
Usheninte Dangana <usheninte234@gmail.com>
2025-12-20 13:04:48 +0100

    reset

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -3,33 +3,33 @@ def translate(text):
 
     statement = ""
 
-    if text[0] in vowels:
-        if text[0] in vowels:
-            statement += text + "ay"
-        elif "xr" in text or "yt" in text:
-            statement += text + "ay"
-    else:
+    # if "qu" in text:
+    #     holder = text.split("qu")
+    #     start = holder.pop(0)
+    #     holder.append(start)
+    #     word = "".join(holder)
 
-        if "qu" in text:
-            holder = text.split("qu")
-            start = holder.pop(0)
-            holder.append(start)
-            word = "".join(holder)
+    #     statement += word
+    #     statement += "qu" + "ay"
+    #     print(word)
+    # else:
+    #     if text[0] in vowels:
+    #         if text[0] in vowels:
+    #             statement += text + "ay"
+    #         elif "xr" in text or "yt" in text:
+    #             statement += text + "ay"
+    #     else:
+    #         holder = []
 
-            statement += "qu" + "ay"
-            print(word)
-        else:
-            holder = []
+    #         for char in text:
+    #             holder.append(char)
 
-            for char in text:
-                holder.append(char)
+    #         while holder[0] not in vowels:
+    #             item = holder.pop(0)
+    #             holder.append(item)
 
-            while holder[0] not in vowels:
-                item = holder.pop(0)
-                holder.append(item)
-
-            statement = "".join(holder)
-            statement += "ay"
+    #         statement = "".join(holder)
+    #         statement += "ay"
 
     return statement
 
