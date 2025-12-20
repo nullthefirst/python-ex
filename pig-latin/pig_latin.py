7a846c515b92fb2dbc96f5000d560eac90c9c751
Usheninte Dangana <usheninte234@gmail.com>
2025-12-20 12:39:50 +0100

    Implemented rule 2

--- pig-latin/pig_latin.py
+++ pig-latin/pig_latin.py
@@ -4,8 +4,27 @@ def translate(text):
     statement = ""
 
     if text[0] in vowels:
-        statement += text + "ay"
-    elif "xr" in text or "yt" in text:
-        statement += text + "ay"
+        if text[0] in vowels:
+            statement += text + "ay"
+        elif "xr" in text or "yt" in text:
+            statement += text + "ay"
+    else:
+        holder = []
+
+        for char in text:
+            holder.append(char)
+
+        while holder[0] not in vowels:
+            item = holder.pop(0)
+            holder.append(item)
+
+        statement = "".join(holder)
+        statement += "ay"
+
+    print(statement)
 
     return statement
+
+translate("pig")
+translate("chair")
+translate("thrush")
